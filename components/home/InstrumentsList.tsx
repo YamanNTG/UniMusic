ea04074cc6c3d3c1fe4fcdd848006c54f37cc325
YamanNTG <claudiuoprea21@gmail.com>
2024-08-27 23:46:20 +0100

    fixed error in InstrumentsList,array missing

--- components/home/InstrumentsList.tsx
+++ components/home/InstrumentsList.tsx
@@ -1,16 +1,15 @@
 import InstrumentCard from "../card/InstrumentCard";
-import type { InstrumentsListProps } from "@/utils/types";
+import type { InstrumentCardProps } from "@/utils/types";
 
-function InstrumentsList({ instruments, children }: InstrumentsListProps) {
+function InstrumentsList({
+  instruments,
+}: {
+  instruments: InstrumentCardProps[];
+}) {
   return (
     <section className="mt-4 gap-8 grid sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4">
       {instruments.map((instrument) => {
-        return (
-          <div key={instrument.id}>
-            {children}
-            <InstrumentCard key={instrument.id} instrument={instrument} />
-          </div>
-        );
+        return <InstrumentCard key={instrument.id} instrument={instrument} />;
       })}
     </section>
   );
