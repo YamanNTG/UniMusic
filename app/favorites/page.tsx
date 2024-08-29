8620173b0d8e1ef93741860344e1740c94d33690
YamanNTG <claudiuoprea21@gmail.com>
2024-08-29 12:15:55 +0100

    debug

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -2,10 +2,18 @@ import EmptyList from "@/components/home/EmptyList";
 import InstrumentsList from "@/components/home/InstrumentsList";
 import { fetchFavorites } from "@/utils/actions";
 
-async function FavoritesPage() {
-  const favorites = await fetchFavorites();
-  if (favorites.length === 0) return <EmptyList />;
-  return <InstrumentsList instruments={favorites} />;
+// async function FavoritesPage() {
+//   const favorites = await fetchFavorites();
+//   if (favorites.length === 0) return <EmptyList />;
+//   return <InstrumentsList instruments={favorites} />;
+// }
+
+// export default FavoritesPage;
+
+import React from "react";
+
+function page() {
+  return <div>page</div>;
 }
 
-export default FavoritesPage;
+export default page;
