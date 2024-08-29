163ff3533a094fc9be58ae393e1e7d7d62c6f9ee
YamanNTG <claudiuoprea21@gmail.com>
2024-08-29 12:15:55 +0100

    debug

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
           <Providers>
-            {/* <Navbar /> */}
+            <Navbar />
             <main className="container py-10">{children}</main>
           </Providers>
         </body>
