b3683d1ab87c2f8cbd0e2c32600305a3dac84c2b
YamanNTG <claudiuoprea21@gmail.com>
2024-08-29 11:24:30 +0100

    debug

--- app/instruments/[id]/loading.tsx
+++ app/instruments/[id]/loading.tsx
@@ -1,8 +1,16 @@
 "use client";
 
 import { Skeleton } from "@/components/ui/skeleton";
+// function loading() {
+//   return <Skeleton className="h-[300px] md:h-[500px] w-full rounded" />;
+// }
+
+// export default loading;
+
+import React from "react";
+
 function loading() {
-  return <Skeleton className="h-[300px] md:h-[500px] w-full rounded" />;
+  return <div>loading</div>;
 }
 
 export default loading;
