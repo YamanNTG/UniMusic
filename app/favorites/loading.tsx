0ebe761547df0b607524e73744c44302e5175c25
YamanNTG <claudiuoprea21@gmail.com>
2024-08-29 11:58:31 +0100

    debug

--- app/favorites/loading.tsx
+++ app/favorites/loading.tsx
@@ -2,16 +2,8 @@
 
 import LoadingCards from "@/components/card/LoadingCards";
 
-// function loading() {
-//   return <LoadingCards />;
-// }
-
-// export default loading;
-
-import React from "react";
-
 function loading() {
-  return <div>loading</div>;
+  return <LoadingCards />;
 }
 
 export default loading;
