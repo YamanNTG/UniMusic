51384fc48a7a75179cf484626f69cd0e4ea63604
YamanNTG <claudiuoprea21@gmail.com>
2024-09-02 23:44:37 +0100

    Reviews page and instrument review completed

--- components/reviews/InstrumentReviews.tsx
+++ components/reviews/InstrumentReviews.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { fetchInstrumentReviews } from "@/utils/actions";
+import Title from "../instruments/Title";
+import ReviewCard from "./ReviewCard";
 
-function InstrumentReviews() {
-  return <div>InstrumentReviews</div>;
+async function InstrumentReviews({ instrumentId }: { instrumentId: string }) {
+  const reviews = await fetchInstrumentReviews(instrumentId);
+  if (reviews.length < 1) return null;
+  return (
+    <div className="mt-8">
+      <Title text="Reviews" />
+      <div className="grid md:grid-cols-2 gap-8 mt-4">
+        {reviews.map((review) => {
+          const { comment, rating } = review;
+          const { firstName, profileImage } = review.profile;
+          const reviewInfo = {
+            comment,
+            rating,
+            name: firstName,
+            image: profileImage,
+          };
+          return <ReviewCard key={review.id} reviewInfo={reviewInfo} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default InstrumentReviews;
